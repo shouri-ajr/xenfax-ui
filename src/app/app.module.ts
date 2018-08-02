6299ce5b28447cba0cdcef46073c11e685ecce74
Srinivas Navudu <srinivasn@ajrdc.lan>
2018-08-02 09:23:52 +0530

    Modified Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { LeftnavComponent } from './leftnav/leftnav.component';
-import { NumbersComponent } from './numbers/numbers.component';
 import { DocumentsComponent } from './documents/documents.component';
 import { SignatureModelComponent } from './signature-model/signature-model.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    LeftnavComponent,
-    NumbersComponent,
     DocumentsComponent,
     SignatureModelComponent
   ],
--- src/app/numbers/numbers.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-numbers',
-  templateUrl: './numbers.component.html',
-  styleUrls: [ './numbers.component.scss' ]
-})
-export class NumbersComponent {
-}
